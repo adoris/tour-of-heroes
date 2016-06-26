36a7a654fe61d67bb38be7210d6354940d0abeca
Bill Gauvey <bgauvey@gmail.com>
2016-06-26 09:57:02 -0400

    git update

---
+++ app/heroes/heroes.routes.js
@@ -0,0 +1,9 @@
+"use strict";
+var hero_list_component_1 = require('./hero-list.component');
+var hero_detail_component_1 = require('./hero-detail.component');
+exports.HeroesRoutes = [
+    { path: 'heroes', component: hero_list_component_1.HeroListComponent },
+    { path: 'hero', component: hero_detail_component_1.HeroDetailComponent },
+    { path: 'hero/:id', component: hero_detail_component_1.HeroDetailComponent }
+];
+//# sourceMappingURL=heroes.routes.js.map
